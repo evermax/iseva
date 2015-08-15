3705ded745c4772a57b6906d1a71ff7b75eded9c
Maxime <maxlasserre@free.fr>
2015-08-15 14:42:41 +0200

    Starting point Parse the JSON db file and serve the JSON for the provided URLs. The db file is by default `db.json` (relative path). But it can also be passed using -db arg.

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/evermax/json-server/env"
+)
+
+const (
+	dbPath = "db.json"
+)
+
+var dbFile string
+
+func init() {
+	flag.StringVar(&dbFile, "db", dbPath, "Specify the path of the file in which the JSON is. The default value is db.json")
+}
+
+func main() {
+	flag.Parse()
+
+	env := env.Interface{DB: dbFile}
+	http.HandleFunc("/", env.Handler)
+	fmt.Print("Starting server\n")
+	http.ListenAndServe(":3000", nil)
+}
